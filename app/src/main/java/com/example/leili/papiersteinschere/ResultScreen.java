ff49ac92481f16d5ce9a2a662bda60cadb36948b
Matjap <matleivs@gmail.com>
2018-09-13 13:20:25 +0200

    - testbutton converted to a button that brings you back to the activity_main. - onCLickWinMethod added to show the winner from stone, scissor, paper - getAuswahl Method added shows the value from plaers choice

--- app/src/main/java/com/example/leili/papiersteinschere/ResultScreen.java
+++ app/src/main/java/com/example/leili/papiersteinschere/ResultScreen.java
@@ -1,6 +1,7 @@
 package com.example.leili.papiersteinschere;
 
 import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
@@ -20,25 +21,58 @@ public class ResultScreen extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_resultscreen);
         ButterKnife.bind(this);
-        message = (TextView) (findViewById(R.id.changeText));
+        message = findViewById(R.id.changeText);
     }
 
-    @OnClick(R.id.testButtonPTwo)
-    void onClickTestButtonTwo() {
-        int defValuePlayers = getResources().getInteger(R.integer.defValuePlayers);
-        int auswahlPTwo = sharedPref.getInt(getString(R.string.auswahlPTwo), defValuePlayers);
-        Context context = getApplicationContext();
-        String text = String.valueOf(auswahlPTwo);
-        int duration = Toast.LENGTH_LONG;
-        Toast toast = Toast.makeText(context, text, duration);
-        toast.show();
+    @OnClick(R.id.startNewRound)
+    void onClickStartNewRound() {
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        this.startActivity(intent);
     }
     @OnClick(R.id.winner)
     void onClickWin(){
 
         int defValuePlayers = getResources().getInteger(R.integer.defValuePlayers);
         int auswahlPTwo = sharedPref.getInt(getString(R.string.auswahlPTwo), defValuePlayers);
-        message.setText("funktioniert");
+        int auswahlPOne = sharedPref.getInt("auswahlPOne", defValuePlayers);
+
+        if (auswahlPOne == auswahlPTwo) {
+            message.setText("Draw, beide Player haben die Auswahl " + getAuswahl(auswahlPOne));
+        } else {
+            if (auswahlPOne == 0) {
+                if (auswahlPTwo == 1) {
+                    message.setText("Player1 schlägt Player2 " + getAuswahl(auswahlPOne) + ">" + getAuswahl(auswahlPTwo));
+                } else {
+                    message.setText("Player2 umwickelt Player1 " + getAuswahl(auswahlPTwo) + ">" + getAuswahl(auswahlPOne));
+                }
+            } else if (auswahlPOne == 1) {
+                if (auswahlPTwo == 0) {
+                    message.setText("Player2 schlägt Player1 " + getAuswahl(auswahlPTwo) + ">" + getAuswahl(auswahlPOne));
+                } else {
+                    message.setText("Player1 zerschneidet Player2 " + getAuswahl(auswahlPOne) + ">" + getAuswahl(auswahlPTwo));
+                }
+
+            } else {
+                if (auswahlPTwo == 0) {
+                    message.setText("Player1 umwickelt Player2 " + getAuswahl(auswahlPOne) + ">" + getAuswahl(auswahlPTwo));
+                } else {
+                    message.setText("Player2 zerschneidet Player1 " + getAuswahl(auswahlPTwo) + ">" + getAuswahl(auswahlPOne));
+                }
+            }
+        }
+
+    }
+
+    private String getAuswahl(int auswahlPlayer) {
+        if (auswahlPlayer == 0) {
+            return "Stein";
+        } else if (auswahlPlayer == 1) {
+            return "Schere";
+        } else {
+            return "Papier";
+        }
+
         }
 
 
